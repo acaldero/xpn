726b32c52fdf7cf0d3609d3c15494f3193300bc9
Diego Camarmas <dcamarma@inf.uc3m.es>
2024-07-03 10:16:07 +0200

    Minor updates

--- test/integrity/flush-preload/lanza-dir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -x
-
-export XPN_CONF="${HOME}/expand/test/xpn/xpn.conf.example.xml"
-export MYSERVER_FILE="${HOME}/expand/test/xpn/xpn.dns"
-
-./cp-local2xpn.exe  test.tmp   /PNFS3/tmp/kkk1
-./cp-xpn2local.exe             /PNFS3/tmp/kkk1  test1.tmp
-
